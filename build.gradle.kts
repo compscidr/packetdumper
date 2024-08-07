0de3639209c0c68bbe6b93ac5b8067b70693b118
Jason Ernst <ernstjason1@gmail.com>
2024-08-07 16:15:22 -0700

    Added minimal required gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.kotlinter) apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    plugins {
+        kotlin("jvm") version "2.0.0"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+rootProject.name = "packetdumper"
+include(":packetdumper")
