7e3434fed01171df78f42736a5c66763d4aff7cf
Jason Ernst <ernstjason1@gmail.com>
2024-08-08 12:50:14 -0700

    Prep for making maven central releases

--- packetdumper/build.gradle.kts
+++ packetdumper/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
     alias(libs.plugins.kotlinter)
     id("jacoco")
+    alias(libs.plugins.git.version) // https://stackoverflow.com/a/71212144
+    alias(libs.plugins.sonatype.maven.central)
+    alias(libs.plugins.gradleup.nmcp)
 }
 
 java {
@@ -35,4 +38,56 @@ dependencies {
     testImplementation(libs.bundles.test)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(libs.logback.classic)
+}
+
+version = "0.0.0-SNAPSHOT"
+gitVersioning.apply {
+    refs {
+        branch(".+") { version = "\${ref}-SNAPSHOT" }
+        tag("v(?<version>.*)") { version = "\${ref.version}" }
+    }
+}
+
+// see: https://github.com/vanniktech/gradle-maven-publish-plugin/issues/747#issuecomment-2066762725
+// and: https://github.com/GradleUp/nmcp
+nmcp {
+    val props = project.properties
+    publishAllPublications {
+        username = props["centralPortalToken"] as String? ?: ""
+        password = props["centralPortalPassword"] as String? ?: ""
+        // or if you want to publish automatically
+        publicationType = "AUTOMATIC"
+    }
+}
+
+// see: https://vanniktech.github.io/gradle-maven-publish-plugin/central/#configuring-the-pom
+mavenPublishing {
+    coordinates("com.jasonernst.packetdumper", "packetdumper", version.toString())
+    pom {
+        name = "PacketDumper"
+        description = "A kotlin / android compatible buffer / packet dumper."
+        inceptionYear = "2024"
+        url = "https://github.com/compscidr/packetdumper"
+        licenses {
+            license {
+                name = "GPL-3.0"
+                url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+                distribution = "repo"
+            }
+        }
+        developers {
+            developer {
+                id = "compscidr"
+                name = "Jason Ernst"
+                url = "https://www.jasonernst.com"
+            }
+        }
+        scm {
+            url = "https://github.com/compscidr/packetdumper"
+            connection = "scm:git:git://github.com/compscidr/packetdumper.git"
+            developerConnection = "scm:git:ssh://git@github.com/compscidr/packetdumper.git"
+        }
+    }
+
+    signAllPublications()
 }
