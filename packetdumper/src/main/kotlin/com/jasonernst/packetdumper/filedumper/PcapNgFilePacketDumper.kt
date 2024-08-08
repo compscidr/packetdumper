83ef7d771037fb7d0e3910aa73b8d3ffb09972e9
Jason Ernst <ernstjason1@gmail.com>
2024-08-07 23:19:18 -0700

    WiP: initial commit for Pcap dumpers

---
+++ packetdumper/src/main/kotlin/com/jasonernst/packetdumper/filedumper/PcapNgFilePacketDumper.kt
@@ -0,0 +1,24 @@
+package com.jasonernst.packetdumper.filedumper
+
+import com.jasonernst.packetdumper.EtherType
+import java.nio.ByteBuffer
+
+class PcapNgFilePacketDumper(override var filename: String) : AbstractFilePacketDumper() {
+    override fun open() {
+        TODO("Not yet implemented")
+    }
+
+    override fun close() {
+        TODO("Not yet implemented")
+    }
+
+    override fun dumpBuffer(
+        buffer: ByteBuffer,
+        offset: Int,
+        length: Int,
+        addresses: Boolean,
+        etherType: EtherType?,
+    ) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ packetdumper/src/main/kotlin/com/jasonernst/packetdumper/serverdumper/PcapNgTcpServerPacketDumper.kt
@@ -0,0 +1,28 @@
+package com.jasonernst.packetdumper.serverdumper
+
+import com.jasonernst.packetdumper.EtherType
+import java.nio.ByteBuffer
+
+class PcapNgTcpServerPacketDumper(val listenPort: Int = DEFAULT_PORT) : AbstractServerPacketDumper() {
+    companion object {
+        const val DEFAULT_PORT = 19000
+    }
+
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+
+    override fun dumpBuffer(
+        buffer: ByteBuffer,
+        offset: Int,
+        length: Int,
+        addresses: Boolean,
+        etherType: EtherType?,
+    ) {
+        TODO("Not yet implemented")
+    }
+}
