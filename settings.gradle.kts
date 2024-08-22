5ededa5e0f6728e8a568a695191080afa6db7241
Jason Ernst <ernstjason1@gmail.com>
2024-08-22 10:28:56 -0700

    Working build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,6 @@ pluginManagement {
         mavenCentral()
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
-    plugins {
-<<<<<<< Updated upstream
-        kotlin("jvm") version "2.0.20"
-=======
-        kotlin("jvm") version "2.0.10"
-        alias(libs.plugins.android.application)
-        alias(libs.plugins.jetbrains.kotlin.android)
->>>>>>> Stashed changes
-    }
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
