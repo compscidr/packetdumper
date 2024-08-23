a0b940b2efe122f8b44fcb713178a4a0dbb49419
Jason Ernst <ernstjason1@gmail.com>
2024-08-23 06:00:30 -0700

    composable activity + screen

--- example-android/src/main/java/com/jasonernst/example_android/MainActivity.kt
+++ example-android/src/main/java/com/jasonernst/example_android/MainActivity.kt
@@ -1,4 +1,18 @@
 package com.jasonernst.example_android
 
-class MainActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.jasonernst.example_android.ui.MainScreen
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                MainScreen()
+            }
+        }
+    }
 }
