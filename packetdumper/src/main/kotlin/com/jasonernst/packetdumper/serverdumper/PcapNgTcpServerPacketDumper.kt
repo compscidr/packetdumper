828d68ee000dd366bb6845f7ec80e512b84380fd
Jason Ernst <ernstjason1@gmail.com>
2024-11-20 14:01:14 -0800

    replace ip logging code with library code

--- packetdumper/src/main/kotlin/com/jasonernst/packetdumper/serverdumper/PcapNgTcpServerPacketDumper.kt
+++ packetdumper/src/main/kotlin/com/jasonernst/packetdumper/serverdumper/PcapNgTcpServerPacketDumper.kt
@@ -58,7 +58,7 @@ class PcapNgTcpServerPacketDumper(
                     return@Thread
                 }
                 logger.trace("WiresharkTcpDump listening on {}", socket!!.localSocketAddress)
-                logAllIPAddresses()
+                LogIp.logAllIPAddresses()
                 isRunning.set(true)
 
                 while (isRunning.get()) {
@@ -177,42 +177,5 @@ class PcapNgTcpServerPacketDumper(
 
     private fun issueCallback() {
         callback?.onConnectedUsersChanged(connectionQueue.map { it.remoteSocketAddress.toString() })
-    }
-
-    private fun logAllIPAddresses(
-        excludeInterfaces: List<String> = listOf("bump", "docker", "virbr", "veth", "tailscale", "dummy", "tun"),
-    ) {
-        try {
-            val interfaces = java.net.NetworkInterface.getNetworkInterfaces()
-            if (interfaces == null) {
-                logger.error("No network interfaces found")
-                return
-            }
-            for (networkInterface in interfaces) {
-                var excluded = false
-                for (excludeInterface in excludeInterfaces) {
-                    if (networkInterface.displayName.contains(excludeInterface)) {
-                        excluded = true
-                        break
-                    }
-                }
-                if (excluded) {
-                    continue
-                }
-                if (networkInterface.isUp.not()) {
-                    continue
-                }
-                val addresses = networkInterface.inetAddresses.toList()
-                if (addresses.isEmpty()) {
-                    continue
-                }
-                logger.trace("Network Interface: ${networkInterface.name}")
-                for (inetAddress in addresses) {
-                    logger.trace("  IP Address: ${inetAddress.hostAddress}")
-                }
-            }
-        } catch (e: SocketException) {
-            logger.error("Error getting network interfaces", e)
-        }
     }
 }
